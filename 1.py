8db429f04e2cd264c2e50ec3dfc356f5835f7978
Witek131 <wik131@rambler.ru>
2024-10-15 11:25:30 +0300

    1 и 6 задание

--- 1.py
+++ 1.py
@@ -5,7 +5,7 @@ s1 = '248 157 456 136 23 34 28 17'
 s2={frozenset(x) for x in 'FBE AEH FGH GH AB CD CDB AC'.split()}
 for i in permutations('ABCDEFGH'):
     s3=s1
-    for j in zip('12345678', i) :
+    for j in zip('12345678', i) :# j это все возможные варианты (цифра, буква)
         s3 =s3.replace(j[0], j[1])
     s3= {frozenset(x) for x in s3.split()}
 
